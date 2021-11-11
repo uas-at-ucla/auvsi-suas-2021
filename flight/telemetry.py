27af93a40615defe742d3e7c1136714d37920879
paigelarson <larsonpel181@gmail.com>
2021-11-10 18:59:17 -0800

    Update telemetry.py

--- flight/telemetry.py
+++ flight/telemetry.py
@@ -17,47 +17,55 @@ class TelemetryData:
 
 async def position(drone: System, telemetry_data: TelemetryData):
     """
-    Coroutine to constantly update telemetry_data with latest position data
+    Continue to constantly update telemetry_data with latest position data
     """
-    async for pos in drone.telemetry.Position():
+    async for pos in drone.telemetry.position():
         telemetry_data.latitude = pos.latitude_deg
         telemetry_data.longitude = pos.longitude_deg
         telemetry_data.absolute_altitude = pos.absolute_altitude_m
-        telemetry_data.relative_altitude = pos.relative_altitude_m
+        telemetry_data.relative_altitude = pos.relative_altitude_m  
+        print("Position: {pos}")
 
 async def body(drone:System, telemetry_data: TelemetryData):
     async for turn in drone.telemetry.AngularVelocityBody():
         telemetry_data.roll=turn.roll_rad_s
         telemetry_data.pitch=turn.pitch_rad_s
         telemetry_data.yaw=turn.yaw_rad_s
+        print("Orientation: {turn}")
         
 async def landed(drone: System, telemetry_data: TelemetryData):
     async for is_landed in drone.telemetry.landed_state():
         telemetry_data.is_landed = is_landed
+        print("Is Landed: {is_landed}")
 
 async def air(drone: System, telemetry_data: TelemetryData):
     async for is_in_air in drone.telemetry.in_air():
         telemetry_data.is_in_air = is_in_air
+        print("In Air: {is_in_air}")
         
 async def ground_velocity(drone: System, telemetry_data: TelemetryData):
     async for g_velocity in drone.telemetry.velocity_ned():
         telemetry_data.g_velocity= g_velocity
+        print("Ground Velcoity: {g_velocity}")
 
 async def angular_velocity(drone: System, telemetry_data: TelemetryData):
     async for a_velocity in drone.telemetry.attitude_angular_velocity_body():
-        telemetry_data.a_veloc
-        ity= a_velocity
+        telemetry_data.a_velocity= a_velocity
+        print("Angular Velocity: {a_velocity}")
         
 async def acceleration(drone:System, telemetry_data: TelemetryData):
     async for acc in drone.telemetry.AccelerationFrd():
         telemetry_data.forward=acc.forward_m_s2
         telemetry_data.right=acc.right_m_s2
         telemetry_data.down=acc.down_m_s2
+        print("Acceleration: {acc}")
 
 async def battery(drone:System, telemetry_data: TelemetryData):
     async for battery in drone.telemetry.battery():
         telemetry_data.battery=battery
+        print("Battery: {battery.remaining_percent}")
 
+    
 #telemetry data needed
 '''
 gps coordinates  - check
