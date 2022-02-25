d58055bc15525cf2ae40b5c0cba464ab3a63a458
Paige Larson <larsonpel181@gmail.com>
2022-02-24 19:04:47 -0800

    Update waypoints.py

--- flight/waypoints.py
+++ flight/waypoints.py
@@ -1,42 +1,76 @@
 import asyncio
+
 from mavsdk import System
+
 from telemetry import TelemetryData
 
-class Node:
-    def __init__(self, lat=None, long=None,flying_alt=None, yaw=None):
-        self.lat=lat
-        self.long=long
-        self.flying_alt=flying_alt
-        self.yaw=yaw
-        self.next=None
+from drone import MissionPoint
+
+
+
 class LinkedList:
+
     def __init__(self):
+
         self.head=None
 
-async def toLinkedList(lst,num):
+
+
+    def addToEnd(self,
+
+     point):
+
+        newmp=point
+
+        if self.head is None:
+
+            self.head=newmp
+
+            return
+
+        ptr=self.head
+
+        while(ptr.next != None):
+
+            ptr=ptr.next
+
+        ptr.next=newmp
+
+
+
+
+
+
+
+def createLinkedList(mplist,num):
+
     coords=LinkedList()
-    coords=None
-    for i in range(0,num,1):
-        temp=lst(i)
-        if(coords==None):
-            coords=temp
-        else:
-            ptr=coords
-            while(coords.next!=None):
-                ptr=ptr.next
-            ptr.next=temp
+
+    coords.head=None
+
+    for i in range(0,num,1): 
+
+        coords.addToEnd(mplist[i])
+
     return coords
+
     
-async def waypoints(orig_list):
+
+async def waypoints(orig_list, drone):
+
     
-    drone = System()
-    await drone.connect(system_address="udp://:14540")
-    
+
     i=len(orig_list)
-    fin_list=toLinkedList(orig_list,i)
+
+    fin_list=createLinkedList(orig_list,i)
+
     current=fin_list.head
+
     
+
     while current is not None:
-        drone.action.goto_location(current.lat, current.long, current.flying_alt, current.yaw)
+
+        drone.system.action.goto_location(current.latitude, current.longitude, current.altitude,0)
+
         current=current.next
-    
+
