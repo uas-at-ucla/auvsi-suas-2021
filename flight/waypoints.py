32cee6df9ca3f416224034a18d291dc2bca7286d
Paige Larson <larsonpel181@gmail.com>
2022-02-25 19:02:36 -0800

    Update waypoints.py

--- flight/waypoints.py
+++ flight/waypoints.py
@@ -6,7 +6,7 @@ from drone import MissionPoint
 class LinkedList:
     def __init__(self):
         self.head=None
-        
+
     def addToEnd(self,point):
         newmp=point
         if self.head is None:
@@ -22,13 +22,16 @@ def createLinkedList(mplist,num):
     coords.head=None
     for i in range(0,num,1): 
         coords.addToEnd(mplist[i])
-    return coords  
+    return coords
 
 async def waypoints(orig_list, drone):
+
     i=len(orig_list)
     fin_list=createLinkedList(orig_list,i)
     current=fin_list.head
-    
+
     while current is not None:
-        await drone.system.action.goto_location(current.latitude, current.longitude, current.altitude,0)
+        await drone.goto(current.latitude, current.longitude, current.altitude,0)
         current=current.next
+
+
