4f60b6c5da4a059645af26de248e5c3ea2b932f8
Paige Larson <larsonpel181@gmail.com>
2022-02-25 18:39:17 -0800

    Update waypoints.py

--- flight/waypoints.py
+++ flight/waypoints.py
@@ -1,76 +1,34 @@
 import asyncio
-
 from mavsdk import System
-
 from telemetry import TelemetryData
-
 from drone import MissionPoint
 
-
-
 class LinkedList:
-
     def __init__(self):
-
         self.head=None
-
-
-
-    def addToEnd(self,
-
-     point):
-
+        
+    def addToEnd(self,point):
         newmp=point
-
         if self.head is None:
-
             self.head=newmp
-
             return
-
         ptr=self.head
-
         while(ptr.next != None):
-
             ptr=ptr.next
-
         ptr.next=newmp
 
-
-
-
-
-
-
 def createLinkedList(mplist,num):
-
     coords=LinkedList()
-
     coords.head=None
-
     for i in range(0,num,1): 
-
         coords.addToEnd(mplist[i])
-
-    return coords
-
-    
+    return coords  
 
 async def waypoints(orig_list, drone):
-
-    
-
     i=len(orig_list)
-
     fin_list=createLinkedList(orig_list,i)
-
     current=fin_list.head
-
     
-
     while current is not None:
-
-        drone.system.action.goto_location(current.latitude, current.longitude, current.altitude,0)
-
+        await drone.system.action.goto_location(current.latitude, current.longitude, current.altitude,0)
         current=current.next
-
