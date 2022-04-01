88dfe873b3fcf67cd34e9949bef88c4efcb20fbd
ggharibian <gharibian.garni0@gmail.com>
2022-03-31 17:30:55 -0700

    Deleted unecessary imports
    Deleted an import in the App.js file that was causing errors.

--- ground-javascript/ground-station/src/App.js
+++ ground-javascript/ground-station/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import EntireWindow from './EntireWindow';
 
 function App() {
