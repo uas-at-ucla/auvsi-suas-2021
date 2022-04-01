239f76d5c6b647efba44a6d26931f3942a12f1f3
ggharibian <gharibian.garni0@gmail.com>
2022-03-31 19:46:54 -0700

    Add dummy map for Zoya to use in UI Design
    See Summary

--- ground-javascript/ground-station/src/EntireWindow.js
+++ ground-javascript/ground-station/src/EntireWindow.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import dummy_map from "./images/dummy_map.jpg"
 
 class EntireWindow extends Component {
     render(){
-        return <div></div>
+        return <div className="full_screen">
+            <div className="odm-map">
+                <img src={dummy_map} alt="dronemap"/>
+            </div>
+        </div>
     }
 }
 
