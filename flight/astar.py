b6c3f9f8e6dfc0787da1a4920ddcf0a754bfc4a6
Paige Larson <larsonpel181@gmail.com>
2022-04-22 17:42:51 -0700

    Update astar.py

--- flight/astar.py
+++ flight/astar.py
@@ -2,7 +2,6 @@ import math
 
 import matplotlib.pyplot as plt
 
-show_animation = True
 
 
 class AStar:
@@ -54,17 +53,6 @@ class AStar:
                                                                      open_set[
                                                                          o]))
             cudrent = open_set[c_id]
-
-            # show graph
-            if show_animation:  # pragma: no cover
-                plt.plot(self.calc_grid_position(current.x, self.min_x),
-                         self.calc_grid_position(current.y, self.min_y), "xc")
-                # for stopping simulation with the esc key.
-                plt.gcf().canvas.mpl_connect('key_release_event',
-                                             lambda event: [exit(
-                                                 0) if event.key == 'escape' else None])
-                if len(closed_set.keys()) % 10 == 0:
-                    plt.pause(0.001)
 
             if current.x == goal_node.x and current.y == goal_node.y:
                 print("Find goal")
@@ -203,7 +191,6 @@ class AStar:
 
 
 def main():
-    print(__file__ + " start!!")
 
     # start and goal position
     sx = 10.0  # [m]
@@ -234,17 +221,6 @@ def main():
         ox.append(40.0)
         oy.append(60.0 - i)
 
-    if show_animation:  # pragma: no cover
-        plt.plot(ox, oy, ".k")
-        plt.plot(sx, sy, "og")
-        plt.plot(gx, gy, "xb")
-        plt.grid(True)
-        plt.axis("equal")
 
     a_star = AStar(ox, oy, grid_size, drone_radius)
     rx, ry = a_star.planning(sx, sy, gx, gy)
-
-    if show_animation:  # pragma: no cover
-        plt.plot(rx, ry, "-r")
-        plt.pause(0.001)
-        plt.show()
