7557774d292b4814b975aab8a68dfc19cf7d6ba1
Nathan Wei <njwei@g.ucla.edu>
2022-05-15 17:01:55 -0700

    states working

--- ugv/ugv.py
+++ ugv/ugv.py
@@ -1,9 +1,13 @@
 import asyncio, time
+from xmlrpc.client import SERVER_ERROR
 from mavsdk import System
 from mavsdk import telemetry
+import requests
 
 END_STATE = 2
 DETACH_THRESH = 1 # TODO
+
+SERVER_ROUTE = 'localhost:3000/ugv'
 
 def dist_to_ground():
     return 0.5 # TEMP
@@ -50,33 +54,35 @@ async def run_ugv_mission(target_location, drop_location, drop_bounds):
             break
     
 
-    state = 0
+    requests.post(SERVER_ROUTE+'/state', json= {"state": "STANDBY"})
+    state = requests.get(SERVER_ROUTE+'/state').json()['state']
     
     await asyncio.sleep(1)#420) # Sleep until takeoff guaranteed complete
     
     #get current_location
 
   
-    while state != END_STATE:
+    while state != "COMPLETE":
         current_location = get_current_location(ugv)
-        if state == 0:
+        if state == "STANDBY":
             if (dist_to_ground() < DETACH_THRESH and 
             (abs(current_location[0] - drop_location[0] < drop_bounds) and 
             abs(current_location[1] - drop_location[1] < drop_bounds))):
                 detach_from_uas()
+                requests.post(SERVER_ROUTE+'/state', json= {"state": "DETACHED"})
                 await ugv.action.arm()
                 await ugv.action.takeoff()
                 await asyncio.sleep(10) # Don't drive while tumbling through air
-                state = 1
-        elif state == 1:
+                requests.post(SERVER_ROUTE+'/state', json= {"state": "DRIVING"})
+        elif state == "DRIVING":
             await ugv.action.goto_location(*target_location)
-            state = 2
-        elif state == 2:
+            requests.post(SERVER_ROUTE+'/state', json= {"state": "COMPLETE"})
             report_mission_success()
-
+        state = requests.get(SERVER_ROUTE+'/state'.json()['state'])
 async def get_current_location(ugv):
     async for current_location in ugv.telemetry.position():
-        data = [current_location.latitude_deg, current_location.logitude_deg, 0, 0]
+        data = [current_location.latitude_deg, current_location.longitude_deg, 0, 0]
+        requests.post(SERVER_ROUTE+'/state', json={"latitude":data[0], "longitude":data[1]})
         return data
 
 
