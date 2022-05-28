5bc27bb68d8ee30c5f2437e7d0cda26fb42490d0
Nathan Wei <njwei@g.ucla.edu>
2022-05-27 18:27:03 -0700

    added mission gets

--- ugv/ugv.py
+++ ugv/ugv.py
@@ -3,6 +3,7 @@ from xmlrpc.client import SERVER_ERROR
 from mavsdk import System
 from mavsdk import telemetry
 import requests
+import numpy as np
 
 END_STATE = 2
 DETACH_THRESH = 1 # TODO
@@ -14,7 +15,7 @@ def dist_to_ground():
     pass # TODO
 
 def detach_from_uas():
-    pass # TODO
+    pass # TODO 10sec wait
 
 def report_mission_sucess():
     pass # TODO
@@ -30,7 +31,7 @@ def post(path, data):
     except Exception as e:
         print(e)
         return None
-def get(path, data):
+def get(path):
     try:
         result = requests.get(path)
 
@@ -41,6 +42,15 @@ def get(path, data):
     except Exception as e:
         print(e)
         return None
+
+def point_in_bounds(bounds, current_location):
+    in_bounds = True
+    for i in range(bounds.shape[0]-1):
+        D = (bounds[i+1][0]-bounds[i][0])*(current_location[1]-bounds[i][1]) 
+        - (current_location[0]-bounds[i][0])*(bounds[i+1][1]-bounds[i][1])
+        if D < 0:
+            in_bounds = False
+    return in_bounds
 
 async def termination_checks(ugv): # Run concurrently with main
     time_last_connected = time.time()
@@ -66,7 +76,7 @@ async def termination_checks(ugv): # Run concurrently with main
         
         # TODO: Also terminate if out of bounds
 
-async def run_ugv_mission(target_location, drop_location, drop_bounds):
+async def run_ugv_mission():
     ugv = System()
     await ugv.connect()
     
@@ -77,8 +87,15 @@ async def run_ugv_mission(target_location, drop_location, drop_bounds):
             break
     
 
-    requests.post(SERVER_ROUTE+'/state', json= {"state": "STANDBY"})
-    state = requests.get(SERVER_ROUTE+'/state').json()['state']
+    post(SERVER_ROUTE+'/state', json= {"state": "STANDBY"})
+    state = get(SERVER_ROUTE+'/state').json()['state']
+    target_location = get(SERVER_ROUTE+'/mission').json()['ugvDrivePos']
+    target_location = [target_location['latitude'], target_location['longitude'], 0, 0]
+    drop_location_data = get(SERVER_ROUTE+'/mission').json()['airDropBoundaryPoints']
+    drop_location_data_array = np.empty((len(drop_location_data)+1, 2))
+    for i in range(len(drop_location_data)):
+        drop_location_data_array[i] = np.array([drop_location_data[i]['latitude'], drop_location_data[i]['longitude']])
+    drop_location_data_array[len(drop_location_data)] = drop_location_data_array[0]
     
     await asyncio.sleep(1)#420) # Sleep until takeoff guaranteed complete
     
@@ -88,27 +105,26 @@ async def run_ugv_mission(target_location, drop_location, drop_bounds):
     while state != "COMPLETE":
         current_location = get_current_location(ugv)
         if state == "STANDBY":
-            if (dist_to_ground() < DETACH_THRESH and 
-            (abs(current_location[0] - drop_location[0] < drop_bounds) and 
-            abs(current_location[1] - drop_location[1] < drop_bounds))):
+            if (dist_to_ground() < DETACH_THRESH and point_in_bounds(drop_location_data_array, current_location)):
                 detach_from_uas()
-                requests.post(SERVER_ROUTE+'/state', json= {"state": "DETACHED"})
+                post(SERVER_ROUTE+'/state', json= {"state": "DETACHED"})
+                state = get(SERVER_ROUTE+'/state'.json()['state'])
                 await ugv.action.arm()
                 await ugv.action.takeoff()
-                await asyncio.sleep(10) # Don't drive while tumbling through air
-                requests.post(SERVER_ROUTE+'/state', json= {"state": "DRIVING"})
+                #await asyncio.sleep(10) # Don't drive while tumbling through air
+                post(SERVER_ROUTE+'/state', json= {"state": "DRIVING"})
         elif state == "DRIVING":
             await ugv.action.goto_location(*target_location)
-            requests.post(SERVER_ROUTE+'/state', json= {"state": "COMPLETE"})
-            report_mission_success()
-        state = requests.get(SERVER_ROUTE+'/state'.json()['state'])
+            post(SERVER_ROUTE+'/state', json= {"state": "COMPLETE"})
+            #report_mission_success()
+        state = get(SERVER_ROUTE+'/state'.json()['state'])
 async def get_current_location(ugv):
     async for current_location in ugv.telemetry.position():
         data = [current_location.latitude_deg, current_location.longitude_deg, 0, 0]
-        requests.post(SERVER_ROUTE+'/state', json={"latitude":data[0], "longitude":data[1]})
+        post(SERVER_ROUTE+'/heartbeat', json={"latitude":data[0], "longitude":data[1]})
         return data
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run_ugv_mission([10, 20, 30, 40], [0, 0, 30, 0], .05))
+    loop.run_until_complete(run_ugv_mission())
--- ugv/ugv_server_test.py
+++ ugv/ugv_server_test.py
@@ -1,5 +1,6 @@
 import requests
-
+import numpy as np
+from scipy.spatial import ConvexHull, convex_hull_plot_2d
 # response = requests.get('http://localhost:3000/ugv/state')
 # print(response.text)
 # print(response.json()["state"])
@@ -8,3 +9,34 @@ import requests
 # print(response.text)
 response = requests.get('http://localhost:3000/ugv/mission/')
 print(response.json())
+drop_location_data = response.json()['airDropBoundaryPoints']
+drop_location_data_array = np.empty((len(drop_location_data)+1, 2))
+for i in range(len(drop_location_data)):
+    drop_location_data_array[i] = np.array([drop_location_data[i]['latitude'], drop_location_data[i]['longitude']])
+drop_location_data_array[len(drop_location_data)] = drop_location_data_array[0]
+
+current_location = [38.14616667, -76.4263, 0, 0]
+def point_in_bounds(bounds, current_location):
+    in_bounds = True
+    for i in range(bounds.shape[0]-1):
+        D = (bounds[i+1][0]-bounds[i][0])*(current_location[1]-bounds[i][1]) 
+        - (current_location[0]-bounds[i][0])*(bounds[i+1][1]-bounds[i][1])
+        if D < 0:
+            in_bounds = False
+    return in_bounds
+
+print(drop_location_data_array)
+if point_in_bounds(drop_location_data_array, current_location):
+    print("in bounds")
+else:
+    print("not in bounds")
+
+hull = ConvexHull(drop_location_data_array)
+import matplotlib.pyplot as plt
+plt.plot(drop_location_data_array[:,0], drop_location_data_array[:,1], 'o')
+plt.plot(current_location[0], current_location[1], 'o')
+for simplex in hull.simplices:
+    plt.plot(drop_location_data_array[simplex, 0], drop_location_data_array[simplex, 1], 'k-')
+plt.show()
+#data = json.loads(response.json())
+#print(response.json()['airDropBoundaryPoints'][0])
